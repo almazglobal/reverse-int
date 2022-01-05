c291c0679a06a13c7798e700383513180ebf6723
Alexander Mazurov <almazlocal@gmail.com>
2022-01-05 12:39:55 +0400

    feat: realised reverse of number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let arrNum = n.toString().split('')
+    if (arrNum[0] === '-') {
+        return parseInt(`${arrNum.splice(1).reverse().join('')}`)
+    } else {
+        return parseInt(arrNum.reverse().join(''))
+    }
 }
